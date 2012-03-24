dbaece1c44d417a69cc6a662643d24b05fa358b5
xleroy <xleroy@198b03b0-60d5-47cb-a09e-f81db6e26457>
2012-03-24 08:27:09 +0000

    Improving compatibility with upcoming OCaml 4.00 release: - configure: more robust detection of compare_ext; detection of new hash API - caml_z.c: using the new hash API if available.

--- trunk/caml_z.c
+++ trunk/caml_z.c
@@ -45,6 +45,9 @@ extern "C" {
 #include <caml/intext.h>
 #include <caml/callback.h>
 #include <caml/intext.h>
+#ifdef Z_OCAML_HASH
+#include <caml/hash.h>
+#endif
 
 #define inline __inline
 
@@ -2593,26 +2596,30 @@ int ml_z_custom_compare(value arg1, value arg2)
   return r;
 }
 
+#ifndef Z_OCAML_HASH
+#define caml_hash_mix_uint32(h,n) ((h) * 65599 + (n))
+#endif
 
 static intnat ml_z_custom_hash(value v)
 {
   Z_DECL(v);
   mp_size_t i;
-  intnat acc = 0;
+  uint32 acc = 0;
   Z_CHECK(v);
   Z_ARG(v);
   for (i = 0; i < size_v; i++) {
-    acc = acc * 65599 + ptr_v[i];
+    acc = caml_hash_mix_uint32(acc, (uint32)(ptr_v[i]));
 #ifdef ARCH_SIXTYFOUR
-    acc = acc * 65599 + (ptr_v[i] >> 32);
+    acc = caml_hash_mix_uint32(acc, ptr_v[i] >> 32);
 #endif
   }
-  if (sign_v) acc++;
-#ifdef ARCH_SIXTYFOUR
-  return acc & 0xffffffff;
-#else
-  return acc;
+#ifndef ARCH_SIXTYFOUR
+  /* To obtain the same hash value on 32- and 64-bit platforms */
+  if (size_v % 2 != 0)
+    acc = caml_hash_mix_uint32(acc, 0);
 #endif
+  if (sign_v) acc++;
+  return acc;
 }
 
 CAMLprim value ml_z_hash(value v)
@@ -2633,7 +2640,8 @@ static void ml_z_custom_serialize(value v,
   Z_DECL(v);
   Z_CHECK(v);
   Z_ARG(v);
-  if ((mp_size_t)(uint32) size_v != size_v) failwith("Z.serialize: number is too large");
+  if ((mp_size_t)(uint32) size_v != size_v) 
+    caml_failwith("Z.serialize: number is too large");
   nb = size_v * sizeof(mp_limb_t);
   caml_serialize_int_1(sign_v ? 1 : 0);
   caml_serialize_int_4(nb);
